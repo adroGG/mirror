d78b7d855c096915fff312ea6f8510af165b074c
adri <adriangg@uoc.edu>
2021-03-12 12:03:05 +0100

    Camara, movimiento y animacion
    Instalado el 3D GameKit, Cinemachine y Input System.
    He eliminado muchos assets del 3D GameKit porque no pensaba usarlos, pero he dejado muchos de los scripts para tener referencias de código.
    
    Para implementar la mecánica de espejo he conseguido únicamente que la rotación del personaje se invierta. El "espejo" no es totalmente simétrico por la naturaleza de las animaciones. Habrá que trabajar también en eso.

---
+++ mirrorProject/Assets/Prefabs/CharacterAssets/RealCharacterInputManager.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(PlayerInput))]
+public class RealCharacterInputManager : MonoBehaviour {
+
+    private Animator animator;
+    private GameObject realCharacter;
+    private GameObject reflectedCharacter;
+
+    int isWalkingHash;
+    int isRunningHash;
+    private Vector2 moveDirection; 
+
+    PlayerInput input;
+
+    public void Move(InputAction.CallbackContext context) {
+
+        moveDirection = context.ReadValue<Vector2>();
+        
+        if(context.performed) {
+            ManageMovement(context.ReadValue<Vector2>());
+        }
+
+        if(context.canceled) {
+            ResetAnimations();
+        }
+    }
+
+    void ResetAnimations() {
+        animator.SetBool("isWalking", false);
+        animator.SetBool("isWalkingBackwards", false);
+        animator.SetBool("isRunning", false);
+    }
+
+    void ManageMovement(Vector2 direction) {
+        if(direction[1] < -0.5f) {
+            animator.SetBool("isWalkingBackwards", true);
+        }
+        
+        if(direction[1] > 0.5f) {
+            animator.SetBool("isWalking", true);
+        }
+    }
+
+    void ManageRotation(Vector2 direction) {
+        bool rotaIzq = direction[0] < -0.5;
+        bool rotaDerecha = direction[0] > 0.5;
+        bool anda = direction[1] >= -0.5f;
+        bool andaHaciaAtras = direction[1] < -0.5f;
+
+        if(rotaIzq) { // rotar izquierda
+            ResetAnimations();
+            if(anda) { //rotar con animacion de andar
+                animator.SetBool("isWalking", true);
+                realCharacter.transform.Rotate(Vector3.up * -1 * (100f * Time.deltaTime));
+                reflectedCharacter.transform.Rotate(Vector3.up * (100f * Time.deltaTime));
+            } else if(andaHaciaAtras) { //rotar con animacion de andar hacia atrás
+                animator.SetBool("isWalkingBackwards", true);
+                realCharacter.transform.Rotate(Vector3.up * (100f * Time.deltaTime));
+                reflectedCharacter.transform.Rotate(Vector3.up * -1 * (100f * Time.deltaTime));
+            }
+        }
+
+        if(rotaDerecha) { // rotar derecha
+            ResetAnimations();
+            if(anda) {
+                animator.SetBool("isWalking", true);
+                realCharacter.transform.Rotate(Vector3.up * (100f * Time.deltaTime));
+                reflectedCharacter.transform.Rotate(Vector3.up * -1 * (100f * Time.deltaTime));
+            } else if(andaHaciaAtras) {
+                animator.SetBool("isWalkingBackwards", true);
+                realCharacter.transform.Rotate(Vector3.up * -1 *(100f * Time.deltaTime));
+                reflectedCharacter.transform.Rotate(Vector3.up * (100f * Time.deltaTime));
+            }
+        }
+    }
+
+    private void FixedUpdate() {
+        ManageRotation(moveDirection);
+    }
+
+    void Awake() {
+        
+    }
+
+    void Start() {
+        realCharacter = GameObject.Find("RealCharacter");
+        reflectedCharacter = GameObject.Find("ReflectedCharacter");
+
+        animator = GetComponent<Animator>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+
+    void HandleMovement() {
+        bool isWalkingBackwards = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+    }
+
+    void OnEnable() {
+        // input.CharacterControls.Enable();
+    }
+
+    void OnDisable() {
+        // input.CharacterControls.Disable();
+    }
+}
+
---
+++ mirrorProject/Assets/Prefabs/CharacterAssets/RealPlayerInput.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Prefabs/Characters/RealPlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @RealPlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @RealPlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""RealPlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""CharacterControls"",
+            ""id"": ""4858f3ec-b84a-4a1a-9f32-fad90c05276c"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""a1b7133e-6d13-4215-8052-b1a13de80c4c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD Input"",
+                    ""id"": ""5e570def-ad99-4d87-b741-3a42b06f8a61"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""ae182e57-858c-4a6c-a208-14c2a9ba8310"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""bc857db1-6fc8-497b-b4c4-87002ddf6de6"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""08d45751-b86e-4aa0-a518-181f4e16c50a"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""d1e9d8ba-ec25-46e0-9f95-241381ea1ff9"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // CharacterControls
+        m_CharacterControls = asset.FindActionMap("CharacterControls", throwIfNotFound: true);
+        m_CharacterControls_Movement = m_CharacterControls.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // CharacterControls
+    private readonly InputActionMap m_CharacterControls;
+    private ICharacterControlsActions m_CharacterControlsActionsCallbackInterface;
+    private readonly InputAction m_CharacterControls_Movement;
+    public struct CharacterControlsActions
+    {
+        private @RealPlayerInput m_Wrapper;
+        public CharacterControlsActions(@RealPlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_CharacterControls_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_CharacterControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(CharacterControlsActions set) { return set.Get(); }
+        public void SetCallbacks(ICharacterControlsActions instance)
+        {
+            if (m_Wrapper.m_CharacterControlsActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_CharacterControlsActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_CharacterControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public CharacterControlsActions @CharacterControls => new CharacterControlsActions(this);
+    public interface ICharacterControlsActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}

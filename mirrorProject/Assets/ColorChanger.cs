368325e224fc99ae6fb0a6d12812cf9014bf573f
adri <adriangg@uoc.edu>
2021-03-25 21:56:00 +0100

    Menu principal y de opciones añadidos.
    He añadido estos dos menú y una pequeña animación al fondo para que haya cambios de color y no sea tan estático.

---
+++ mirrorProject/Assets/ColorChanger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChanger : MonoBehaviour
+{
+
+    Image img;
+
+    private  float speed = 0.5f;
+    [SerializeField] [Range(0f, 1f)] float lerpTime;
+    [SerializeField] Color[] targetColors;
+
+    private int colorIndex = 0;
+    private float t = 0f;
+    private int len;
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+        len = targetColors.Length;
+    }
+
+    void FixedUpdate()
+    {
+        // Mathf.PingPong(Time.deltaTime * speed, 1.0f)
+        img.color = Color.Lerp(img.color, targetColors[colorIndex], lerpTime * Time.deltaTime);
+
+        t = Mathf.Lerp(t, 1f, lerpTime * Time.deltaTime);
+
+        if (t > 0.9f)
+        {
+            t = 0f;
+            colorIndex++;
+            colorIndex = (colorIndex >= len) ? 0 : colorIndex;
+
+        }
+
+    }
+}

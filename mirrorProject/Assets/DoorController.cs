9eeb7e1cff399f1d579bb5a44ac7698a7079f4bc
adri <adriangg@uoc.edu>
2021-04-19 19:52:02 +0200

    Añadidos sistemas de particulas.
    He añadido sistemas de partículas para que se perciba que la apertura de la puerta es un evento importante. También hay un sistema de partículas en los triggers que abren las puerta.

--- mirrorProject/Assets/DoorController.cs
+++ mirrorProject/Assets/DoorController.cs
@@ -7,9 +7,13 @@ public class DoorController : MonoBehaviour
 
     public bool isOpen;
     Animator doorAnimator;
+    ParticleSystem openDoorParticles;
 
     void Start() {
         doorAnimator = transform.GetComponent<Animator>();
+        openDoorParticles = transform.GetComponentInChildren<ParticleSystem>();
+
+        Debug.Log("Particle system: " + openDoorParticles);
     }
 
     public void ManageDoor() {
@@ -17,10 +21,12 @@ public class DoorController : MonoBehaviour
             isOpen = false;
             Debug.Log("Door Closed");
             doorAnimator.SetBool("character_nearby", false);
+            openDoorParticles.Stop();
         } else {
             isOpen = true;
             Debug.Log("Door Opened");
             doorAnimator.SetBool("character_nearby", true);
+            openDoorParticles.Play();
         }
     }
 
--- mirrorProject/Assets/OpenDoor.cs
+++ mirrorProject/Assets/OpenDoor.cs
@@ -1,19 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
-public class OpenDoor : MonoBehaviour
-{
+public class OpenDoor : MonoBehaviour {
     public UnityEvent interactAction;
 
-    void OnTriggerEnter() {
-        Debug.Log("Entrando en el cubo. Ahora debe abrirse la puerta.");
-        interactAction.Invoke();
+    private Collider col;
+
+    // añadir un bool para que la puerta permanezca abierta hasta que algo salga
+
+    void OnTriggerEnter(Collider other) {
+        // Preventing diferent colliders to open and close doors
+        if(col == null) {
+            col = other;
+            interactAction.Invoke();
+            Debug.Log("Entrando en el cubo. Ahora debe abrirse la puerta.");
+        }
     }
 
-    void OnTriggerExit()
-    {
-        Debug.Log("Saliendo del cubo. Ahora debe cerrarse la puerta.");
-        interactAction.Invoke();
+    void OnTriggerExit(Collider other) {
+        // Preventing diferent colliders to open and close doors
+        if (col != null) {
+            Debug.Log("Saliendo del cubo. Ahora debe cerrarse la puerta.");
+            interactAction.Invoke();
+            col = null;
+        }
     }
 
 }

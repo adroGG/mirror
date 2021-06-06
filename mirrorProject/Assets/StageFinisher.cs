9694006de6d80af286207abe73c002044be4f357
adri <adriangg@uoc.edu>
2021-06-06 22:45:38 +0200

    Añadida una espera de 5 segundos para cerrar el ultimo nivel

---
+++ mirrorProject/Assets/StageFinisher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageFinisher : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(FinishLevel());
+    }
+
+    private IEnumerator FinishLevel() {
+        yield return new WaitForSeconds(5);
+        Application.Quit();
+    }
+
+}
